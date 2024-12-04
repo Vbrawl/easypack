60770bd1f5a47b0abc11bf63076c0c71f78be457
VBrawl <konstantosjim@gmail.com>
2024-12-04 16:04:16 +0200

    Fixed spaces and extended function test

--- tests/string_array.c
+++ tests/string_array.c
@@ -5,26 +5,32 @@
 
 
 int test_getNextString() {
-    struct sarray arr = {0};
-    const char *hi = "hi";
-    const char *hello = "hello";
+  struct sarray arr = {0};
+  const char *hi = "hi";
+  const char *hello = "hello";
 
-    sarray_addString(&arr, hi, strlen(hi));
-    sarray_addString(&arr, hello, strlen(hello));
+  sarray_addString(&arr, hi, strlen(hi));
+  sarray_addString(&arr, hello, strlen(hello));
 
-    char *cursor = sarray_getNextString(&arr, NULL);
-    if(strcmp(cursor, hi) != 0) {
-        sarray_clearAll(&arr);
-        return 1;
-    }
+  char *cursor = sarray_getNextString(&arr, NULL);
+  if(strcmp(cursor, hi) != 0) {
+    sarray_clearAll(&arr);
+    return 1;
+  }
 
-    cursor = sarray_getNextString(&arr, cursor);
-    if(strcmp(cursor, hello) != 0) {
-        sarray_clearAll(&arr);
-        return 2;
-    }
+  cursor = sarray_getNextString(&arr, cursor);
+  if(strcmp(cursor, hello) != 0) {
+    sarray_clearAll(&arr);
+    return 2;
+  }
 
-    return 0;
+  cursor = sarray_getNextString(&arr, cursor);
+  if(cursor != NULL) {
+    sarray_clearAll(&arr);
+    return 3;
+  }
+
+  return 0;
 }
 
 
