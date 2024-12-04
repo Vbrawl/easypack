6814a8751279778151a0cc0dcfebc96531fb30d8
VBrawl <konstantosjim@gmail.com>
2024-12-04 22:15:22 +0200

    Added support for autorun.easypack files

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <limits.h>
+#include <sys/stat.h>
 #include "utils.h"
 #include "string_array.h"
 
@@ -57,11 +58,13 @@ int extractPackage(const char *out_dir) {
   return 0;
 }
 
-int main() {
+int main(int argc, char *const *argv) {
   uint32_t data_size = 0;
   const char *root = "awd";
   const char *out_name = "easypack.new";
   const char *out_dir = "dumped";
+  const char *autorun_name = "autorun.easypack";
+  int err = 0;
 
   data_size = getEmbeddedDataSize(NULL);
   printf("Data Size: %u\n", data_size);
@@ -71,6 +74,12 @@ int main() {
   }
   else {
     extractPackage(out_dir);
+    chdir(out_dir);
+    if(access(autorun_name, F_OK) == 0) {
+      chmod(autorun_name, S_IWUSR | S_IRUSR | S_IXUSR | S_IWGRP | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+      execv(autorun_name, argv);
+      perror("main -> execv");
+    }
   }
 
   return 0;
