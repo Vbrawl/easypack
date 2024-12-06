72b549d2e5a76bb89c805aa3751a15662e7db8b4
VBrawl <konstantosjim@gmail.com>
2024-12-06 17:39:23 +0200

    Added ARC_EXECUTABLES parameter and README file

--- addons/autorun_creator/autorun_creator.c
+++ addons/autorun_creator/autorun_creator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <easypack_addon.h>
+#include "easypack_addon.h"
+#include "string_array.h"
 #include <string.h>
 #include <stdlib.h>
 
@@ -7,28 +8,75 @@
 #define AUTORUN_DATA \
 "#!/bin/sh\n"\
 "F=\"%s\"\n"\
-"chmod +x \"$F\"\n"\
+"chmod +x \"$F\" %s\n"\
 "./$F"
 
 #define ENV_AUTORUNCREATOR_FILENAME "ARC_FILE"
+#define ENV_AUTORUNCREATOR_BINARIES "ARC_EXECUTABLES"
 
+char* quotedJoin(struct sarray *list) {
+  char *data = NULL, *tmp = NULL, *cursor = NULL;
+  size_t doffset = 0, csize = 0;
+  size_t dsize = list->buf_size + (list->count * 2);
+
+  if(dsize == 0) {
+    data = malloc(1);
+    *data = '\0';
+    return data;
+  }
+
+  tmp = data = malloc(dsize);
+  while((cursor = sarray_getNextString(list, cursor)) != NULL) {
+    if(tmp != data) {
+      *tmp = ' ';
+      tmp++;
+    }
+    csize = strlen(cursor);
+
+    *tmp = '"';
+    tmp++;
+    strncpy(tmp, cursor, csize);
+    tmp += csize;
+    *tmp = '"';
+    tmp++;
+  }
+  data[dsize] = '\0';
+  return data;
+}
 
 int execute(struct fs *system) {
-  const char *executable = getenv(ENV_AUTORUNCREATOR_FILENAME);
+  const char *executable = NULL, *executables_list = NULL;
+  struct sarray executables = {0};
+  char *data = NULL, *executables_quoted_list = NULL;
+  size_t dsize = 0;
+
+  executable = getenv(ENV_AUTORUNCREATOR_FILENAME);
   if(executable == NULL) {
     printf("Define \"%s\" environment variable to proceed!\n");
     return -1;
   }
 
-  size_t dsize = snprintf(NULL, 0, AUTORUN_DATA, executable);
-  char *data = malloc(dsize + 1);
+  executables_list = getenv(ENV_AUTORUNCREATOR_BINARIES);
+  if(executables_list != 0) {
+    if(sarray_addStringsFromList(&executables, executables_list, ',') != 0) {
+      return -1;
+    }
+  }
+  executables_quoted_list = quotedJoin(&executables);
+
+  dsize = snprintf(NULL, 0, AUTORUN_DATA, executable, executables_quoted_list);
+  data = malloc(dsize + 1);
   if(data == NULL) {
     printf("Not enough memory!");
+    free(executables_quoted_list);
+    sarray_clearAll(&executables);
     return -1;
   }
-  snprintf(data, dsize + 1, AUTORUN_DATA, executable);
+  snprintf(data, dsize + 1, AUTORUN_DATA, executable, executables_quoted_list);
 
   addFileToFileSystem(system, AUTORUN_FILENAME, data, dsize);
   free(data);
+  free(executables_quoted_list);
+  sarray_clearAll(&executables);
   return 0;
 }
