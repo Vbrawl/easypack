9cba6c4f6917fa9b5848930dd0e41da72ad12401
VBrawl <konstantosjim@gmail.com>
2024-12-09 14:42:18 +0200

    Added test case

--- tests/utils.c
+++ tests/utils.c
@@ -31,11 +31,31 @@ int test_splitOnce() {
   return 0;
 }
 
+int test_pathJoin() {
+  const char *fullpath = "tmp/subdirectory";
+  char *p1 = NULL, *p2 = NULL, *res = NULL;
+
+  if(splitOnce(fullpath, strlen(fullpath), &p1, &p2, '/') != 0) return 1;
+  res = pathJoin(p1, p2);
+
+  if(strcmp(res, fullpath) != 0) {
+    free(p1);
+    free(p2);
+    free(res);
+    return 2;
+  }
+
+  free(p1);
+  free(p2);
+  free(res);
+  return 0;
+}
 
 int main() {
   int status = 0;
   int counter = 0;
 
   RUN_TEST(status, counter, TEST_INCREMENTOR, test_splitOnce);
+  RUN_TEST(status, counter, TEST_INCREMENTOR, test_pathJoin);
   return status;
 }
