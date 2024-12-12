41c80502b9378e6992b2e4459d58fb3731ba63c5
VBrawl <konstantosjim@gmail.com>
2024-12-12 17:25:12 +0200

    Updated execAndReplace to pass command line parameters to autorun.easypack

--- src/forward.windows.c
+++ src/forward.windows.c
@@ -1,5 +1,6 @@
 #include "forward.h"
 #include <Windows.h>
+#include <stdio.h>
 
 int setEnvironmentVariable(const char* name, const char* value) {
   if (SetEnvironmentVariable(name, value) == 0) {
@@ -15,14 +16,20 @@ int markAsExecutable(const char* name) {
 }
 
 void execAndReplace(const char* name, char* const* argv) {
-  size_t i = 0;
+  // TODO: We need to write a proper parser for generating command lines
+  size_t argv0_size = strlen(argv[0]) + 2; // NOTE: argv is always quoted when in cmdline
+  char* cmdline = GetCommandLine();
+  size_t new_cmdline_size = snprintf(NULL, 0, "%s %s", name, cmdline + argv0_size);
+  char* new_cmdline = malloc(new_cmdline_size + 1);
+  snprintf(new_cmdline, new_cmdline_size + 1, "%s %s", name, cmdline + argv0_size);
+  DWORD exitCode = 0;
 
   STARTUPINFO si = {0}; si.cb = sizeof(si);
   PROCESS_INFORMATION pi = {0};
 
   if (CreateProcess(
       name,
-      GetCommandLine(),
+      new_cmdline,
       NULL,
       NULL,
       FALSE,
@@ -32,10 +39,15 @@ void execAndReplace(const char* name, char* const* argv) {
       &si,
       &pi) == 0) {
     printf("execAndReplace(): %lu\n", GetLastError());
+    free(new_cmdline);
     return -1;
   }
+  free(new_cmdline);
 
+  // wait for process to complete
+  WaitForSingleObject(pi.hProcess, INFINITE);
+  GetExitCodeProcess(pi.hProcess, &exitCode);
   CloseHandle(pi.hProcess);
   CloseHandle(pi.hThread);
-  exit(0);
+  exit(exitCode);
 }
